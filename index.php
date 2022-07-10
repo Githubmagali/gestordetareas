6b39acc7a12c1e0c3f70696d2e0cecfa7923a199
Githubmagali <magalivictoria85068@gmail.com>
2022-07-09 22:25:50 -0300

    cambios

--- index.php
+++ index.php
@@ -9,23 +9,23 @@ $aTareas= json_decode($strJson, true);}
 else
 { $aTareas = array();}
 
-if(isset($_GET["id"]))
-{$id=$_GET["id"];}
+if(isset($_GET["id"])) //GET variable de la query string, si viene la def
+{$id=$_GET["id"];} //si no la dejo como vacio
 else
 {$id="";}
 
 
-if($_POST)
+if($_POST) //definimos las variables donde van a estar almacenado todos los dato
 {$titulo=$_POST["txtTitulo"];
 $prioridad=$_POST["lstPrioridad"];
 $usuario=$_POST["lstUsuario"];
 $estado=$_POST["lstEstado"];
 $descripcion=$_POST["txtDescripcion"];
 
-if($id>=0) //id es para editar tareas
+if($id>=0) //id es para editar tareas EDITO SI ELID ES MAYOR O IGUAL QUE0
 
 {$aTareas[$id]=array(
-    "fecha"=> $aTareas[$id]["fecha"],
+    "fecha"=> $aTareas[$id]["fecha"], //se mantiene la fecha
     "prioridad"=>$prioridad,
     "usuario"=>$usuario,
     "estado"=> $estado,
@@ -60,8 +60,8 @@ header("Location: index.php");}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestor de tareas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
 </head>
 <body>
     <main class="container">
@@ -143,8 +143,8 @@ header("Location: index.php");}
                                     <td><?php echo $tarea["usuario"];?></td>
                                     <td><?php echo $tarea["estado"];?></td>
                                     <td>
-                                    <a href="?id=<?php echo $pos?>&do=eliminar"class="btn btn-success"><i class=" fa-solid fa-trash"></i></a>
-                                    <a href="?id=<?php echo $pos?>"class="btn btn-warning"> <i class="btn fa-solid fa-pen"> </i></a> </td>
+                                    <a href="?id=<?php echo $pos?>&do=eliminar"class="btn "><i class=" fa-solid fa-trash"></i></a>
+                                    <a href="?id=<?php echo $pos?>"> <i class="btn fa-solid fa-pen"> </i></a> </td>
                                 </tr>
                                 <?php }?>
                         </tbody>
